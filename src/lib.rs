6351ba6aaee4e47478c1d8435ec459b8fe4d4590
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-05-08 17:43:46 +0800

    feat(cli): create empty window

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,2 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+pub const WIDTH: u32 = 64;
+pub const HEIGHT: u32 = 32;
---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use pixels::{Pixels, SurfaceTexture};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+pub fn main() {
+    let event_loop = EventLoop::new();
+    let window = {
+        let min_size = LogicalSize::new(chip8::WIDTH as f64, chip8::HEIGHT as f64);
+        let scaled_size = LogicalSize::new(chip8::WIDTH as f64 * 3.0, chip8::HEIGHT as f64 * 3.0);
+        WindowBuilder::new()
+            .with_title("CHIP-8 Emulator")
+            .with_inner_size(scaled_size)
+            .with_min_inner_size(min_size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(chip8::WIDTH, chip8::HEIGHT, surface_texture).unwrap()
+    };
+
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent { event, .. } => match event {
+            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        Event::MainEventsCleared => {
+            window.request_redraw();
+        }
+        Event::RedrawRequested(_) => {
+            pixels.render().unwrap();
+        }
+        _ => {}
+    });
+}
