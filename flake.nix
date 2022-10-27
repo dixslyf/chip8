9b09aff8f9db05d4840e78d5320064df310735d7
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2022-10-27 16:49:01 +0800

    feat(devshell): init
    Uses Nix flakes.

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in
+      with pkgs; rec {
+        devShell = mkShell rec {
+          buildInputs = [
+            libxkbcommon
+            libGL
+            vulkan-loader
+            wayland
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      });
+}
