3822f8ad248b132e4738d3d25d544dff326553ce
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-04-22 21:04:54 +0800

    refactor(devshell): use rust overlay

--- flake.nix
+++ flake.nix
@@ -2,19 +2,29 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
+    rust-overlay,
     ...
   }:
     flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {inherit system;};
+      overlays = [(import rust-overlay)];
+      pkgs = import nixpkgs {inherit system overlays;};
     in
       with pkgs; rec {
         devShell = mkShell rec {
+          nativeBuildInputs = [rust-bin.stable.latest.default];
           buildInputs = [
             alsa-lib
             cmake
