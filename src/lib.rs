4e92320e2381b30fb5ea0e1b8fbabedc795095c8
Dixon Sean Low Yan Feng <dixonseanlow@protonmail.com>
2025-04-22 22:17:42 +0800

    fix(lib): fix incorrect 8xyE opcode behaviour
    The implementation had two problems:
    1. Like 8xy6, it did not account for the scenarios where 0xF is passed
       as x or y.
    2. It set the incorrect value for 0xF. Instead of applying an AND with
       0b1000_0000, it should have been a right shift by 7 to extract the
       most significant bit.

--- src/lib.rs
+++ src/lib.rs
@@ -364,8 +364,12 @@ impl Chip8 {
     /// Sets `vx` to `vy` shifted left by one bit. `vf` is set to the most significant bit of `vy`
     /// prior to the shift.
     fn op_8xye(&mut self, x: u8, y: u8) {
-        self.v[0xF] = self.v[y as usize] & 0b1000_0000;
-        self.v[x as usize] = self.v[y as usize] << 1;
+        // It is possible that 0xF is passsed as x or y, so
+        // we store the old value of y in a separate variable
+        // and mutate vF last.
+        let old_vy = self.v[y as usize];
+        self.v[x as usize] = old_vy << 1;
+        self.v[0xF] = old_vy >> 7;
         self.pc += 2;
     }
 
