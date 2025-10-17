aa8d7936102a5249d3f7a1488dade28c47574737
Dixon Sean Low Yan Feng <root@dixslyf.dev>
2025-10-17 12:11:40 +0800

    build(nix): use crane for building with nix

--- flake.nix
+++ flake.nix
@@ -1,53 +1,110 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
     flake-utils.url = "github:numtide/flake-utils";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
     };
   };
 
   outputs =
     {
+      self,
       nixpkgs,
+      crane,
       flake-utils,
-      rust-overlay,
+      advisory-db,
       ...
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
+        pkgs = nixpkgs.legacyPackages.${system};
         inherit (pkgs) lib;
 
-        buildInputs = with pkgs; [
-          alsa-lib
-          cmake
-          pkg-config
-          fontconfig
-          libxkbcommon
-          libGL
-          vulkan-loader
-          wayland
-          xorg.libXcursor
-          xorg.libXrandr
-          xorg.libXi
-          xorg.libX11
-        ];
-      in
-      {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = [
-            pkgs.rust-bin.stable.latest.default
+        craneLib = crane.mkLib pkgs;
+        src = craneLib.cleanCargoSource ./.;
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          buildInputs = with pkgs; [
+            alsa-lib
+            libxkbcommon
+            libGL
+            vulkan-loader
+            wayland
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
           ];
 
-          inherit buildInputs;
+          nativeBuildInputs = with pkgs; [
+            cmake
+            pkg-config
+            fontconfig
+          ];
+        };
 
-          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+        crate = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            doCheck = false;
+          }
+        );
+      in
+      {
+        checks = {
+          inherit crate;
+
+          crate-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          crate-doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+            }
+          );
+
+          crate-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          crate-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          crate-deny = craneLib.cargoDeny {
+            inherit src;
+          };
+        };
+
+        packages = {
+          default = crate;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = crate;
+        };
+
+        devShells.default = craneLib.devShell {
+          checks = self.checks.${system};
+
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+          # CMAKE_POLICY_VERSION_MINIMUM = "3.5"; # Needed for expat-sys crate
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath commonArgs.buildInputs}";
         };
       }
     );
